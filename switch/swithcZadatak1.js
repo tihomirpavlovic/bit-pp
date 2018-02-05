d7dde99ec09c55c4ec551f2cca99a5b2134027ba
BIT student <classroom@bgit.rs>
2018-02-05 16:32:19 +0100

    switch zadatak1

---
+++ switch/swithcZadatak1.js
@@ -0,0 +1,25 @@
+var grade = "a";
+switch (grade) {
+    case "A":
+    case "a":
+        console.log("Good Job");
+        break;
+    case "B":
+        console.log("Pretty Good");
+        break;
+    case "C":
+        console.log("Passed");
+        break;
+    case "D":
+        console.log("Not so Good");
+        break;
+    case "F":
+        console.log("Failed");
+        break;
+    default:
+        console.log("Unknown grade");
+        break;
+
+}
+
+
